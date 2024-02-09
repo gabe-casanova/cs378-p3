37285e43ecf15ae18435316ef8490df6d4e3d372
gabe-casanova <gabecasanova7@gmail.com>
2024-02-09 16:31:19 -0600

    updated logo path

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,6 +1,6 @@
 // logo credit: http://tinyurl.com/shibaskitchenlogo
 const Logo = () => {
-  return <img src="/images/logo.png" alt="Shiba's Kitchen Logo" id="logo" />;
+  return <img src="images/logo.png" alt="Shiba's Kitchen Logo" id="logo" />;
 };
 
 export default Logo;
